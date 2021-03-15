3ee6c78c7142a280d3c93ed82e185cf9b6195c78
jaywcjlove <398188662@qq.com>
2021-03-15 23:24:42 +0800

    Add example 1.

---
+++ example/demo1/Demo1/Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Demo
+//
+//  Created by 王楚江 on 2021/3/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
